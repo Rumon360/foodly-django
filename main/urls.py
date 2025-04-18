acff9ecb92a48223d896b52137b14ddd27f6f845
Rumon360 <hmk.rumon@gmail.com>
2025-04-18 21:12:52 +0600

    chore: setup tailwind

--- main/urls.py
+++ main/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from . import views
 
-urlpatterns = [path("admin/", admin.site.urls), path("", views.home, name="home")]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("__reload__/", include("django_browser_reload.urls")),
+    path("", views.home, name="home"),
+]
